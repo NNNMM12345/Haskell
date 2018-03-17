602dba35cf6db6ccc83ef7a0dd9b3e3e2c307bab
NNNMM12345 <nikolai1177@outlook.com>
2018-03-16 20:06:36 -0700

    First steps in Haskell

---
+++ Project1.hs
@@ -0,0 +1,17 @@
+import Data.List
+
+{-square x = x*x-}
+
+-- doubleSmallNumber 200 = 200
+-- doubleSmallNumber 10 = 20
+-- doubleSmallNumber 100 = 200
+-- doubleSmallNumber 101 = 101
+-- if the argument is greater than 100, return the argument.
+-- otherwise multiply by 2
+{-doubleSmallNumber x =
+  if x<100 -- here we need a boolean value
+  then x 
+  else 7387583-}
+  
+  
+result = sort [64,22,92,49,2]
